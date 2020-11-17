c511cbfa8c9c5d65b66da30eff4d7c6eb0928a57
Jakob Ruckel <jakob@ruckel.de>
2020-11-17 21:38:35 +0100

    Use collapse(2) instead of collapse(3)
    collapse(3) might cause race conditions

--- source/matmult.cpp
+++ source/matmult.cpp
@@ -69,7 +69,7 @@ void matmult_serial(float **A, float **B, float **C, int d1, int d2, int d3) {
 void matmult_parallel(float **A, float **B, float **C, int d1, int d2, int d3) {
   printf("Perform parallel matrix multiplication...\n");
 
-#pragma omp parallel for collapse(3)
+#pragma omp parallel for collapse(2)
   for (int i = 0; i < d1; i++)
     for (int j = 0; j < d3; j++)
       for (int k = 0; k < d2; k++) {
